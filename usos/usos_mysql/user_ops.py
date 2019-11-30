201a49f30839ac87e58aff7cc89786127581eb5e
bruteus <brute.us@protonmail.com>
2019-11-30 16:35:32 +0100

    Functionality to getting specific users from DB

--- usos/usos_mysql/user_ops.py
+++ usos/usos_mysql/user_ops.py
@@ -2,10 +2,11 @@ from db.db_connector import DBConnector
 from usos.objects.user import User
 
 
-def get_usos_users(connector: DBConnector) -> list:
+def get_usos_users(connector: DBConnector, user_ids: list = None) -> list:
     """Get all users from DB and convert to User objects
 
     :param connector: MySQL DB connector
+    :param user_ids: List of user_ids to get from DB. Omit this parameter if you want to get all users
     :returns: List of User objects
     """
 
@@ -15,12 +16,19 @@ def get_usos_users(connector: DBConnector) -> list:
         'usos_first_name', 'usos_last_name', 'usos_id', 'usos_courses',
         'usos_token', 'usos_token_secret', 'locale', 'is_registered'
     ]
-    query = 'select {} from users;'.format(', '.join(columns))
 
     users = []
-
     cursor = connector.connection.cursor(dictionary=True)
-    cursor.execute(query)
+
+    if user_ids is None:
+        query = 'select {} from users;'.format(', '.join(columns))
+        cursor.execute(query)
+    else:
+        query = 'select {} from users where id in ({});'.format(
+            ', '.join(columns), ', '.join(['%s' for i in range(len(user_ids))])
+        )
+        cursor.execute(query, user_ids)
+
     for user_data in cursor:
         if user_data['is_registered']:
             users.append(User(**user_data))
