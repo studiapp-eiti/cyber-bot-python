53e57c6600046688d871f3e6246d2c2fee856791
bruteus <brute.us@protonmail.com>
2019-11-30 16:35:58 +0100

    Script for updating user usos_id, usos_first_name and usos_last_name in DB

---
+++ scripts/fill_usos_user_info.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append('../')
+
+from dotenv import load_dotenv
+from db.db_connector import DBConnector
+from argparse import ArgumentParser
+from usos.objects.user import User
+from usos.usos_api_calls import get_user_courses, get_user_usos_id_and_name
+from usos.usos_mysql.user_ops import get_usos_users
+from mysql.connector.errors import Error
+
+if __name__ == '__main__':
+    argparser = ArgumentParser(
+        description='Get user USOS ID, first and last name from USOS and fill `users` table in DB'
+    )
+    argparser.add_argument(
+        'user_ids', type=int, metavar='UIDs', nargs='+',
+        help='User IDs from `users` table'
+    )
+    args = argparser.parse_args()
+
+    load_dotenv()
+    connector = DBConnector()
+    connector.connection.autocommit = False
+
+    User.get_usos_api_key()
+    users = get_usos_users(connector, args.user_ids)
+    if len(users) != len(args.user_ids):
+        print('Attempt to update user that doesn\'t exist!')
+        sys.exit(-1)
+
+    cursor = connector.connection.cursor()
+    try:
+        for uid, user in zip(args.user_ids, users):
+            courses = get_user_courses(user)
+            usos_info = get_user_usos_id_and_name(user)
+            insert_usos_info = 'update users ' \
+                               'set usos_id = %s, usos_first_name = %s, usos_last_name = %s, usos_courses = %s ' \
+                               'where id = %s;'
+            cursor.execute(insert_usos_info, (
+                usos_info['id'], usos_info['first_name'], usos_info['last_name'],
+                ';'.join([c.course_id for c in courses]), uid
+            ))
+
+        connector.connection.commit()
+    except Error as err:
+        print(err.msg)
+        connector.connection.rollback()
+    finally:
+        cursor.close()
