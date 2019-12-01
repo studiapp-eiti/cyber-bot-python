51e13a08aefb997120afc140962a7dfd2a7ce4a4
Gustaw Daczkowski <gucio.lektor@gmail.com>
2019-12-01 15:48:31 +0100

    Add supported subjects validation.

--- cron_scripts/scrapper.py
+++ cron_scripts/scrapper.py
@@ -5,15 +5,27 @@ from dotenv import load_dotenv
 from studia3.studia_requests import Studia3Client
 
 
-def scrap():
+def init():
     load_dotenv()
+
+
+def get_supported_subjects():
     db = db_connector.DBConnector()
     q = Queries(db)
-    cookies = q.carry_transaction(lambda: q.get_cookies())
-    if cookies:
-        for cookie in cookies:
-            print(Studia3Client.get_all_courses_urls(cookie["cookie"]))
+    result = q.carry_transaction(q.get_accessible_subject)
+    supported_subjects = dict()
+    for line in result:
+        supported_subjects[line["cookie"]] = line["usos_courses"].split(";")
+    return supported_subjects
+
+
+def check_if_subject_supported(subjects, subject_id):
+    return next((k for k, v in subjects.items() if subject_id in v), False)
 
 
 if __name__ == "__main__":
-    scrap()
+    init()
+    supported_subjects = get_supported_subjects()
+    supported = check_if_subject_supported(supported_subjects, "34")
+    if supported:
+        print("This sub is supported")
--- studia3/studia3_mysql/queries.py
+++ studia3/studia3_mysql/queries.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import mysql.connector.errors
 
+
 class Queries:
     def __init__(self, db):
         """
@@ -31,11 +32,15 @@ class Queries:
         result = self.cursor.fetchall()
         return result
 
+    def get_accessible_subject(self):
+        query = "SELECT studia3_sessions.maintainer_id, studia3_sessions.cookie, u.usos_courses " \
+                "FROM studia3_sessions LEFT JOIN users u on studia3_sessions.maintainer_id = u.id"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
     def update_cookies(self, m_id, expiration_date=None):
         if expiration_date is None:
             self.cursor.execute(f"UPDATE studia3_sessions SET cookie=NULL WHERE maintainer_id = {m_id}")
         else:
             self.cursor.execute(
                 f"UPDATE studia3_sessions SET expires = FROM_UNIXTIME({expiration_date}) WHERE maintainer_id = {m_id} ")
-
-
