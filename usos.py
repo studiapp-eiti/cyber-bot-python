23fed99570c88d63c4d88d5cf351d32b48eb56d7
bruteus <brute.us@protonmail.com>
2019-12-16 21:28:30 +0100

    Small changes

--- usos.py
+++ usos.py
@@ -1,8 +1,7 @@
-import requests
+import json
 from dotenv import load_dotenv
 from usos.usos_api_calls import *
 from usos.objects.user import User
-from usos.usos_mysql.update_tables import update_usos_courses, update_usos_programs, update_new_usos_points
 from usos.usos_mysql.user_ops import get_usos_users
 
 if __name__ == '__main__':
@@ -11,40 +10,7 @@ if __name__ == '__main__':
     User.get_usos_api_key()
     users = get_usos_users()
 
-    courses = set()
-    programs = set()
-    points = set()
-    for u in users:
-        try:
-            programs.update(get_user_programs(u))
-            courses.update(get_user_courses(u))
-            points.update(get_user_points(u))
-        except requests.exceptions.ConnectionError as err:
-            print(err)
-
-    print('User programs:')
-    for i in sorted(programs, key=lambda x: x.program_name_pl):
-        i: Program
-        print(i.program_id, i.program_name_pl, sep=' - ')
-
-    print('\nUser courses:')
-    for i in sorted(courses, key=lambda x: x.course_name_pl):
-        i: Course
-        print(i.course_id, i.course_name_pl, sep=' - ')
-
-    print('\nUser points:')
-    for i in sorted(points, key=lambda x: x.name):
-        i: Points
-        print(i.course_id, i.name, i.points, sep=' - ')
-
-    print('\nUpdating user_programs table...')
-    update_usos_programs(programs)
-    print('Done.')
-
-    print('\nUpdating user_courses table...')
-    update_usos_courses(courses,)
-    print('Done.')
-
-    print('\nUpdating user_points table...')
-    update_new_usos_points(points)
-    print('Done.')
+    for user in users:
+        print('\nTimetable for user: {} {}'.format(user.fb_first_name, user.fb_last_name))
+        tt = get_timetable_for_tommorow(user)
+        # print(json.dumps(tt, indent=2, ensure_ascii=False))
