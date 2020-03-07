44a6384369f9d0cffa83b4e22f23378d07bf70e0
8ru73u5 <brute.us@protonmail.com>
2020-03-07 03:27:03 +0100

    Cleanup

--- usos.py
+++ usos.py
@@ -1,50 +1,24 @@
-import requests
 from dotenv import load_dotenv
-from usos.usos_api_calls import *
-from usos.objects.user import User
-from usos.usos_mysql.update_tables import update_usos_courses, update_usos_programs, update_new_usos_points
-from usos.usos_mysql.user_ops import get_usos_users
+from usos.api_calls import *
+from usos.db.user_ops import get_usos_users
 
 if __name__ == '__main__':
     load_dotenv()
 
-    User.get_usos_api_key()
     users = get_usos_users()
+    timetables = []
 
-    courses = set()
-    programs = set()
-    points = set()
-    for u in users:
-        try:
-            programs.update(get_user_programs(u))
-            courses.update(get_user_courses(u))
-            points.update(get_user_points(u))
-        except requests.exceptions.ConnectionError as err:
-            print(err)
+    for user in users:
+        print('\nTimetable for user: {} {}'.format(user.fb_first_name, user.fb_last_name))
+        tt = get_timetable_for_tomorrow(user)
+        if tt.is_day_off():
+            print('Day off')
+        else:
+            print('Day start: {}\nDay end: {}'.format(tt.start_activity.start_str, tt.end_activity.end_str))
+            print('Gaps:')
+            for g in tt.gaps:
+                print('-> from {} to {} [{}]'.format(g.start_str, g.end_str, g.duration_str))
 
-    print('User programs:')
-    for i in sorted(programs, key=lambda x: x.program_name_pl):
-        i: Program
-        print(i.program_id, i.program_name_pl, sep=' - ')
-
-    print('\nUser courses:')
-    for i in sorted(courses, key=lambda x: x.course_name_pl):
-        i: Course
-        print(i.course_id, i.course_name_pl, sep=' - ')
-
-    print('\nUser points:')
-    for i in sorted(points, key=lambda x: x.name):
-        i: Points
-        print(i.course_id, i.name, i.points, sep=' - ')
-
-    print('\nUpdating user_programs table...')
-    update_usos_programs(programs)
-    print('Done.')
-
-    print('\nUpdating user_courses table...')
-    update_usos_courses(courses,)
-    print('Done.')
-
-    print('\nUpdating user_points table...')
-    update_new_usos_points(points)
-    print('Done.')
+            print('Activities:')
+            for a in tt.activities:
+                print('-> from {} to {} - {} [{}] in {}'.format(a.start.str, a.end_str, a.name_pl, a.type_pl, a.room))
